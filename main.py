eb7173c188d1a78091b88a1dfd14380438a8f99c
johanbergfalk <97jobe34@gmail.com>
2021-03-25 21:33:42 +0100

    part 0

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import sys
+from PyQt5.QtWidgets import *
+
 
 if __name__ == '__main__':
-    print("hello python!")
 
-    print("hello everybody its tacotime")
+    //configs the application window, set up
+    app = QApplication(sys.argv)
+
+
+    //when user closes window the app will exit
+    sys.exit(app.exec())
+
