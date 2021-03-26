d1f695b8158ef8a6d99c0fc064254dc1c7d40198
johanbergfalk <97jobe34@gmail.com>
2021-03-26 21:17:19 +0100

    part 1

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sys
 from PyQt5.QtWidgets import *
 
+from node_editor_wnd import NodeEditorWnd
+
 
 if __name__ == '__main__':
 
-    //configs the application window, set up
+    #configs the application window, set up
     app = QApplication(sys.argv)
 
+    wnd = NodeEditorWnd()
 
-    //when user closes window the app will exit
+    #when user closes window the app will exit
     sys.exit(app.exec())
 
